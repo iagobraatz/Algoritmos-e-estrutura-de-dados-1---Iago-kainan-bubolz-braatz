0cb5e64b761a22494d754b1ec40b181e187e0e7b
iagobraatz <iagobraatz.21@gmail.com>
2025-11-07 15:09:18 -0300

    Create 94.c

---
+++ LeetCode/94.c
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+void inorder(struct TreeNode* root, int* result, int* index) {
+    if (root == NULL) return;
+
+    inorder(root->left, result, index);     
+    result[(*index)++] = root->val;         
+    inorder(root->right, result, index);    
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+    int* result = (int*)malloc(100 * sizeof(int));
+    *returnSize = 0;
+
+    inorder(root, result, returnSize);
+
+    return result;
+}
+//iago kainan bubolz braatz
